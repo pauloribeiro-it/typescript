2ab01ae53ce0d8a4450e14613848948294511545
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-12-29 22:30:51 -0200

    Enums

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+var LanguageKnown;
+(function (LanguageKnown) {
+    LanguageKnown[LanguageKnown["English"] = 0] = "English";
+    LanguageKnown[LanguageKnown["Spanish"] = 1] = "Spanish";
+    LanguageKnown[LanguageKnown["French"] = 2] = "French";
+    LanguageKnown[LanguageKnown["Portuguese"] = 3] = "Portuguese";
+})(LanguageKnown || (LanguageKnown = {}));
 var student = {
     Name: "Paulo",
     Age: 24,
-    PhoneNumber: 123456789
+    PhoneNumber: 123456789,
+    Language: LanguageKnown[LanguageKnown.Portuguese]
 };
 var studentList = [
-    { Name: "Pessoa1", Age: 20, PhoneNumber: 121212121 },
-    { Name: "Pessoa2", Age: 21, PhoneNumber: 131313131 },
-    { Name: "Pessoa3", Age: 22, PhoneNumber: 141414141 }
+    { Name: "Pessoa1", Age: 20, PhoneNumber: 121212121, Language: LanguageKnown[LanguageKnown.English] },
+    { Name: "Pessoa2", Age: 21, PhoneNumber: 131313131, Language: LanguageKnown[LanguageKnown.Spanish] },
+    { Name: "Pessoa3", Age: 22, PhoneNumber: 141414141, Language: LanguageKnown[LanguageKnown.French] }
 ];
 studentList.push(student);
 for (var index = 0; index < studentList.length; index++) {
--- app.ts
+++ app.ts
@@ -1,13 +1,18 @@
+enum LanguageKnown{
+    English,Spanish, French, Portuguese
+}
+
 let student = {
     Name : "Paulo",
     Age: 24,
-    PhoneNumber : 123456789
+    PhoneNumber : 123456789,
+    Language: LanguageKnown[LanguageKnown.Portuguese]
 }
 
 let studentList = [
-    {Name:"Pessoa1",Age:20,PhoneNumber:121212121},
-    {Name:"Pessoa2",Age:21,PhoneNumber:131313131},
-    {Name:"Pessoa3",Age:22,PhoneNumber:141414141}
+    {Name:"Pessoa1",Age:20,PhoneNumber:121212121, Language: LanguageKnown[LanguageKnown.English]},
+    {Name:"Pessoa2",Age:21,PhoneNumber:131313131, Language: LanguageKnown[LanguageKnown.Spanish]},
+    {Name:"Pessoa3",Age:22,PhoneNumber:141414141, Language: LanguageKnown[LanguageKnown.French]}
 ]
 
 studentList.push(student)
