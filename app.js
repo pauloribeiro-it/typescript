1ef13479898427ed2a971536d5e841b110ae135a
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2017-12-29 22:23:06 -0200

    Array

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var a = 10;
-console.log(a);
+var student = {
+    Name: "Paulo",
+    Age: 24,
+    PhoneNumber: 123456789
+};
+var studentList = [
+    { Name: "Pessoa1", Age: 20, PhoneNumber: 121212121 },
+    { Name: "Pessoa2", Age: 21, PhoneNumber: 131313131 },
+    { Name: "Pessoa3", Age: 22, PhoneNumber: 141414141 }
+];
+studentList.push(student);
+for (var index = 0; index < studentList.length; index++) {
+    var element = studentList[index];
+    console.log(element);
+}
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,2 +1,18 @@
-let a:number = 10;
-console.log(a);
+let student = {
+    Name : "Paulo",
+    Age: 24,
+    PhoneNumber : 123456789
+}
+
+let studentList = [
+    {Name:"Pessoa1",Age:20,PhoneNumber:121212121},
+    {Name:"Pessoa2",Age:21,PhoneNumber:131313131},
+    {Name:"Pessoa3",Age:22,PhoneNumber:141414141}
+]
+
+studentList.push(student)
+
+for (let index = 0; index < studentList.length; index++) {
+    var element = studentList[index];
+    console.log(element);
+}
