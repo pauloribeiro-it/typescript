9b9fef5c555812ec4d771b7ddc1405df5c574310
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2018-01-02 21:51:49 -0200

    First part of typescript lang

--- typescriptlang/app.js
+++ typescriptlang/app.js
@@ -2,3 +2,35 @@ var fullName = 'Bob Bobbington';
 var age = 37;
 var sentence = "Hello, my name is " + fullName + ".\n\nI'll be " + (age + 1) + " years old next month.";
 console.log(sentence);
+//tuple
+var x;
+// Initialize it
+x = ["hello", 10]; // OK
+console.log(x[0]);
+//void function
+function warnUser() {
+    alert("This is my warning message");
+}
+//undefined and null
+var u = undefined;
+var n = null;
+// NO RETURN POSSIBILITY
+// Function returning never must have unreachable end point
+function error(message) {
+    throw new Error(message);
+}
+// Inferred return type is never
+function fail() {
+    return error("Something failed");
+}
+// Function returning never must have unreachable end point
+function infiniteLoop() {
+    while (true) {
+    }
+}
+//CAST
+var someValue = "this is a string";
+var strLength = someValue.length;
+var strLengthAs = someValue.length;
+console.log(strLength);
+console.log(strLengthAs);
--- typescriptlang/app.ts
+++ typescriptlang/app.ts
@@ -5,3 +5,48 @@ let sentence: string = `Hello, my name is ${ fullName }.
 I'll be ${ age + 1 } years old next month.`;
 console.log(sentence);
 
+
+//tuple
+let x: [string, number];
+// Initialize it
+x = ["hello", 10]; // OK
+console.log(x[0]);
+
+//void function
+function warnUser(): void {
+    alert("This is my warning message");
+}
+
+//undefined and null
+let u: undefined = undefined;
+let n: null = null;
+
+// NO RETURN POSSIBILITY
+// Function returning never must have unreachable end point
+function error(message: string): never {
+    throw new Error(message);
+}
+
+// Inferred return type is never
+function fail() {
+    return error("Something failed");
+}
+
+// Function returning never must have unreachable end point
+function infiniteLoop(): never {
+    while (true) {
+    }
+}
+
+
+//CAST
+
+let someValue: any = "this is a string";
+
+let strLength: number = (<string>someValue).length;
+
+let strLengthAs: number = (someValue as string).length;
+
+console.log(strLength);
+
+console.log(strLengthAs);
