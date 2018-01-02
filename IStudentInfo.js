22aa499d3bd0527778c3130632d5bd28905f3529
Paulo Ribeiro <pauloroberto.ar14@gmail.com>
2018-01-02 20:04:32 -0200

    Ajustes nos diretorios

--- IStudentInfo.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//export {IStudentInfo as StudentInfo}; 
-//# sourceMappingURL=IStudentInfo.js.map
--- IStudentInfo.ts
+++
@@ -1,8 +0,0 @@
-export default interface IStudentInfo{
-    Name: string,
-    Age: Number,
-    PhoneNumber: Number,
-    Language: string
-}
-
-//export {IStudentInfo as StudentInfo};
